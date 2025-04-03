968ca3854e517090e98a717f8ff2591596a10a4b
Lisa-CtrlShiftZ <heesterslisa@gmail.com>
2025-04-03 09:38:55 +0200

    Created textarea component (not final) Emptied db

---
+++ src/app/components/textarea/textarea.component.ts
@@ -0,0 +1,45 @@
+import { Component, forwardRef, input, output } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'app-textarea',
+  templateUrl: './textarea.component.html',
+  styleUrl: './textarea.component.scss',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => TextareaComponent),
+      multi: true,
+    },
+  ],
+})
+export class TextareaComponent implements ControlValueAccessor {
+  
+  name = input<string>(); 
+  displayName = input<string>();
+  value = input<string>(''); 
+
+  valueChange = output<string>();
+
+  onChange: (value: any) => void = () => {};
+  onTouched: () => void = () => {};
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  onInputChange(event: Event) {
+    const newValue = (event.target as HTMLTextAreaElement).value;
+    this.value();  
+    this.onChange(newValue);
+    this.valueChange.emit(newValue);
+  }
+}
